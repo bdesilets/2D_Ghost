ae820477c7ab5ff0dcb6281a2b2274dedc6c531e
Skyler <bethanydesilets@gmail.com>
2021-11-11 20:20:02 -0500

    Added tree and demon art, fixed layers on collision boxes

---
+++ Enemies/Demon.gd
@@ -0,0 +1,7 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+const MAX_SPEED = 50
+
+func _process(_delta):
+	pass
--- Player/Player.gd
+++ Player/Player.gd
@@ -3,11 +3,11 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 const MAX_SPEED = 50
 
-func _physics_process(delta):
+func _process(_delta):
 	var input_velocity = Vector2.ZERO
-	
+
 	input_velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	velocity = input_velocity.normalized()
-	
-	move_and_collide(velocity * delta * MAX_SPEED) 
+
+	velocity = move_and_slide(velocity * MAX_SPEED) 
