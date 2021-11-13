45bc91a6d97c2322341840b536f628adecd18ae7
Skyler <bethanydesilets@gmail.com>
2021-11-12 20:51:15 -0500

    Added demon chase movment, re-sized sprites

--- Enemies/Demon.gd
+++ Enemies/Demon.gd
@@ -1,7 +1,21 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-const MAX_SPEED = 50
+var MAX_SPEED = null
+var player = null
+
 
 func _process(_delta):
-	pass
+	if player != null:
+		velocity = position.direction_to(player.position) * MAX_SPEED
+	velocity = move_and_slide(velocity)
+
+
+func _on_Player_detector_body_entered(body):
+	MAX_SPEED = 30
+	player = body
+
+
+func _on_Player_detector_body_exited(body):
+	MAX_SPEED = 10
+	velocity = Vector2.ZERO
