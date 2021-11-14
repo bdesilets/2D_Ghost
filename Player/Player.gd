ee1cadcf31c722a6baa167f68ce61cc498f63171
Skyler <bethanydesilets@gmail.com>
2021-11-14 12:18:17 -0500

    Added attack animations and state machine

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,14 +3,25 @@ extends KinematicBody2D
 const MAX_SPEED = 30
 
 var velocity = Vector2.ZERO
+var state = MOVE
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
-enum {MOVE, ATTACK, STAND}
+
+enum {MOVE, ATTACK, DASH}
 
 func _ready():
 	animationTree.active = true
 
 func _process(_delta):
+	match state:
+		MOVE:
+			move_state()
+		ATTACK:
+			attack_state()
+		DASH:
+			dash_state()
+	
+func move_state():
 	var input_velocity = Vector2.ZERO
 
 	input_velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -20,8 +31,21 @@ func _process(_delta):
 	if input_velocity != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_velocity)
 		animationTree.set("parameters/Float/blend_position", input_velocity)
+		animationTree.set("parameters/Attack/blend_position", input_velocity)
 		animationState.travel("Float")
 	else:
 		animationState.travel("Idle")
 		
-	velocity = move_and_slide(velocity * MAX_SPEED) 
+	velocity = move_and_slide(velocity * MAX_SPEED)
+	
+	if Input.is_action_just_pressed("Attack"):
+		state = ATTACK
+
+func attack_state():
+	animationState.travel("Attack")
+	
+func dash_state():
+	pass
+
+func attack_animation_finished():
+	state = MOVE
