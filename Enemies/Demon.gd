3c8be6e87e24baf8d4754ddc97383ad969bb3f19
Skyler <bethanydesilets@gmail.com>
2021-11-14 18:33:00 -0500

    Added demon pixel set and idle animation

--- Enemies/Demon.gd
+++ Enemies/Demon.gd
@@ -1,14 +1,38 @@
 extends KinematicBody2D
 
-var velocity = Vector2.ZERO
 var MAX_SPEED = null
+var velocity = Vector2.ZERO
+var state = MOVE
 var player = null
 
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+enum {MOVE, ATTACK}
+
+func _ready():
+	animationTree.active = true
 
 func _process(_delta):
+	match state:
+		MOVE:
+			move_state()
+		ATTACK:
+			attack_state()
+	
+	
+	
+func move_state():
 	if player != null:
 		velocity = position.direction_to(player.position) * MAX_SPEED
+	
+	animationTree.set("parameters/Idle/blend_position", velocity)
+	animationState.travel("Idle")
+		
 	velocity = move_and_slide(velocity)
+		
+func attack_state():
+	pass
 
 
 func _on_Player_detector_body_entered(body):
