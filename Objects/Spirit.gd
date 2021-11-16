b5221bde0592d53c753cd5b0b8f1c8236118d004
Skyler <bethanydesilets@gmail.com>
2021-11-15 20:05:17 -0500

    Added spirit freed animation, removed collison of spirit and player

--- Objects/Spirit.gd
+++ Objects/Spirit.gd
@@ -1,5 +1,17 @@
 extends StaticBody2D
 
+onready var animation_player = $AnimationPlayer
+onready var animated_sprite =$AnimatedSprite
 
-func _on_playerDetecor_body_entered(body):
+func _on_playerDetecor_body_entered(_body):
+	animated_sprite.visible = true
+	animated_sprite.play("Spirit_freed")
+	animation_player.play("collected")
+
+
+func _on_AnimationPlayer_animation_finished(_anim_name):
+	queue_free()
+
+
+func _on_AnimatedSprite_animation_finished():
 	queue_free()
