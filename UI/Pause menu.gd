06f408387849259eabcef56369af21a4df64909a
skyler <bethany.desilets@gmail.com>
2022-03-09 16:15:50 -0500

    Added start menu

--- UI/Pause menu.gd
+++ UI/Pause menu.gd
@@ -1,20 +1,23 @@
-extends Control
-
-var is_paused = false setget set_is_paused
-
-func unhandled_input(event):
-	if event.if_action_pressed("pause"):
-		self.is_paused = !is_paused
-
-func set_is_paused(value):
-	is_paused = value
-	get_tree().paused = is_paused
-	visible = is_paused
-
-
-func _on_Resume_button_pressed():
-	self.is_paused = false
-
-
-func _on_Quit_button_pressed():
-	get_tree().quit()
+#extends Control
+#
+#func _ready():
+#	$VBoxContainer/Resume_button.grab_focus()
+#
+#var is_paused = false setget set_is_paused
+#
+#func unhandled_input(event):
+#	if event.if_action_pressed("pause"):
+#		self.is_paused = !is_paused
+#
+#func set_is_paused(value):
+#	is_paused = value
+#	get_tree().paused = is_paused
+#	visible = is_paused
+#
+#
+#func _on_Resume_button_pressed():
+#	self.is_paused = false
+#
+#
+#func _on_Quit_button_pressed():
+#	get_tree().quit()
---
+++ UI/Start_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _ready():
+	$VBoxContainer/Start_button.grab_focus()
+
+func _on_Start_button_pressed():
+	get_tree().change_scene("res://World/World.tscn")
+
+
+func _on_Options_button_pressed():
+	var options = load("res://UI/Options_menu.tscn").instance()
+	get_tree().current_scene.add_child(options)
+
+
+func _on_Quit_button_pressed():
+	get_tree().quit()
